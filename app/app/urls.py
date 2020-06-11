c0abe4de0a6b704d4075ca6cf1fe13fc6cf85f6b
Simon Martineau <simon.martineau.3@ulaval.ca>
2020-06-10 21:47:49 -0400

    Flake8 config and travis CI

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,4 +1,4 @@
-"""django_rest_tdd URL Configuration
+"""app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
